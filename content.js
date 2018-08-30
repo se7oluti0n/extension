c7da04a99bf8b03d53a4c8e53b21e1ba9e4698ee
Manh Truong Tuan <manhattan.vn@gmail.com>
2018-08-30 10:46:36 +0900

    save data to csv

--- content.js
+++ content.js
@@ -1,18 +1,31 @@
 var port = chrome.runtime.connect();
 console.log("Content script loaded");
+
+
+
 chrome.runtime.onMessage.addListener(
-  function(request, sender) {
+  function(request, sender, sendResponse) {
     console.log("Contentscript has received a message from from background script: '" + request.message + "'");
-    dataTable = $('#editGraphTable tr')
-    var csv = []; 
-    for (var i = 2; i < dataTable.length; ++i) {
-      row = dataTable[i];
-      date = row.cells[0].children[0].innerText;
-      start1 = row.cells[2].children[0].innerText;
-      start2 = row.cells[2].children[1].innerText;
-      end1 = row.cells[3].children[0].innerText;
-      end2 = row.cells[3].children[1].innerText;
-      csv.push({data, start1, start2, end1, end2});
+    console.log(sender.tab ?
+      "from a content script:" + sender.tab.url :
+      "from the extension");
+    if (request.message == 'getData')  {
+      dataTable = $('#editGraphTable tr')
+      var csv = []; 
+      for (var i = 2; i < dataTable.length; ++i) {
+        row = dataTable[i];
+        date = row.cells[0].children[0].innerText;
+        start1 = row.cells[2].children[0].innerText;
+        start2 = row.cells[2].children[1].innerText;
+        end1 = row.cells[3].children[0].innerText;
+        end2 = row.cells[3].children[1].innerText;
+        csv.push({date: date, start1: start1, start2: start2, end1: end1, end2: end2});
+      }
+      
+      console.log(sendResponse);
+      sendResponse({data: csv});
     }
+
+
  });
 
--- popup.js
+++ popup.js
@@ -7,10 +7,36 @@ let changeColor = document.getElementById('changeColor');
 
   changeColor.onclick = function(element) {
     let color = element.target.value;
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {message: "hello"}, function(response) {
-        console.log(response.farewell);
-      })
+      chrome.tabs.sendMessage(tabs[0].id, {message: "getData"}, function(response) {
+        console.log(response.data);
+
+        var csv_text = 'Date, start1, start2, end1, end2\n';
+
+        for (var i = 0; i < response.data.length; ++i) {
+          day = response.data[i]
+          console.log(day);
+          csv_text += (day['date'] + ', ' + day['start1'] + ', '
+                   + day['start2'] + ', '
+                   + day['end1'] + ', '
+                   + day['end2'] + '\n');
+        }
+
+        //pageList = document.getElementById('csv_content');
+       // pageList.innerHTML = csv_text;
+
+        var result = JSON.stringify(response.data);
+
+    // Save as file
+        var url = 'data:text/plain;base64,' + btoa(csv_text);
+        chrome.downloads.download({
+            url: url,
+            filename: 'timesheet.csv'
+        });
+        
+      });
+
     });
     
   };
